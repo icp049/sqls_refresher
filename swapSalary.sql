0387dce35bf96cabbf2b09a04e0d506f2645f7c8
IJP <108185265+icp049@users.noreply.github.com>
2023-08-31 01:35:30 -0600

    Update swapSalary.sql

--- swapSalary.sql
+++ swapSalary.sql
@@ -1,4 +1,4 @@
-asasasasasasasasasasasasasasasasasasasSELECT actor_id, director_id
+asasasasasasasasasasasasasasasasasasasasSELECT actor_id, director_id
 FROM ActorDirector
 GROUP BY actor_id,director_id
 HAVING COUNT(*) >= 3;
